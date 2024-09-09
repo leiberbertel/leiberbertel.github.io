123e3c8881ac329e31bb5a98a71602e073926d5a
leiber bertel <leiberbertel777@gmail.com>
2024-09-08 21:41:55 -0500

    feat: update info profile and curriculum

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,15 +164,19 @@ $form.addEventListener("submit", handleSubmit);
 async function handleSubmit(event) {
   event.preventDefault();
   const form = new FormData(this);
-  const response = await fetch(this.action, {
-    method: this.method,
-    body: form,
-    headers: {
-      'Accept': "application/json"
+  if (form.keys.length !== 0) {
+    const response = await fetch(this.action, {
+      method: this.method,
+      body: form,
+      headers: {
+        'Accept': "application/json"
+      }
+    })
+    if (response.ok) {
+      this.reset();
+      Swal.fire('¡Te escribiré pronto!', 'Gracias por tu interes en contactarme.', 'success');
     }
-  })
-  if (response.ok) {
-    this.reset();
-    Swal.fire('¡Te escribiré pronto!', 'Gracias por tu interes en contactarme.', 'success');
+  } else {
+    Swal.fire('¡Advertencia!', 'Por favor, recuerda adjuntar tus datos.', 'warning');
   }
 }
